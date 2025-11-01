acdca3a88081b348812d19f6593ff7b2e89b6972
MhmdJhdz <jihadakbardoang@gmail.com>
2025-11-01 11:48:11 +0700

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -48,8 +48,8 @@ export const ArrowRightIcon: React.FC<IconProps> = ({ className }) => (
     </svg>
 );
 
-export const BookmarkIcon: React.FC<IconProps> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+export const BookmarkIcon: React.FC<IconProps> = ({ className, filled = false }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={filled ? "currentColor" : "none"} stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
         <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
     </svg>
 );
